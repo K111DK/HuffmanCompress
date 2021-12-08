567f13a0961abd186805548bf63bca53adbed7d6
loginfinite <74436798+loginfinite@users.noreply.github.com>
2021-12-08 09:58:19 +0800

    Project start

---
+++ cmake-build-debug/CMakeFiles/HuffmanCompress.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuffmanCompress.dir/main.c.obj"
+  "HuffmanCompress.exe"
+  "HuffmanCompress.exe.manifest"
+  "HuffmanCompress.pdb"
+  "libHuffmanCompress.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HuffmanCompress.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HuffmanCompress.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HuffmanCompress.
+# This may be replaced when dependencies are built.
